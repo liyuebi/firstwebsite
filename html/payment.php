e79feffb368f484607f67bd141f4b82a78729ab4
liyuebi <dragon8763@sina.com>
2017-05-13 02:22:19 +0800

    add function to add payment info

--- html/payment.php
+++ html/payment.php
@@ -1,12 +1,50 @@
 <?php
 
-session_start();
 if (!isset($_COOKIE['isLogin']) || !$_COOKIE['isLogin']) {	
 	$home_url = '../index.php';
 	header('Location: ' . $home_url);
 	exit();
 }
 
+session_start();
+$userid = $_SESSION["userId"];
+
+$weAcc = '';
+$isWechatSet = false;
+$aliAcc = '';
+$isAlipaySet = false;
+$bankAcc = '';
+$isBankSet = false;
+
+include "../php/database.php";
+$con = connectToDB();
+if (!$con) {
+	return;
+}
+$res1 = mysql_query("select * from WechatAccount where UserId='$userid'");
+if ($res1) {
+	if (mysql_num_rows($res1) > 0) {
+		$row1 = mysql_fetch_assoc($res1);
+		$weAcc = $row1["WechatAcc"];
+		$isWechatSet = true;
+	}
+}
+$res2 = mysql_query("select * from AlipayAccount where UserId='$userid'");
+if ($res2) {
+	if (mysql_num_rows($res2) > 0) {
+		$row2 = mysql_fetch_assoc($res2);
+		$aliAcc = $row2["AlipayAcc"];
+		$isAlipaySet = true;
+	}
+}
+$res3 = mysql_query("select * from BankAccount where UserId='$userid'");
+if ($res3) {
+	if (mysql_num_rows($res3) > 0) {
+		$row3 = mysql_fetch_assoc($res3);
+		$bankAcc = $row3["AccName"] . " " . $row3["BankAcc"] . " " . $row3["BankName"] . " " . $row3["BankBranch"];
+		$isBankSet = true;
+	}
+}
 
 ?>
 
@@ -35,10 +73,18 @@ if (!isset($_COOKIE['isLogin']) || !$_COOKIE['isLogin']) {
 					return;
 				}
 				
+				if (!confirm("一旦设置，将不能手动修改，是否继续设置微信支付账号？")) {
+					return;
+				}
+				
 				$.post("../php/payaccount.php", {"func":"setwechat","acc":account}, function(data){
 					
 					if (data.error == "false") {
 						alert("设置成功！");	
+						document.getElementById("wechat_unset").style.display = "none";
+						document.getElementById("wechat_set").style.display = "inline";
+						
+						document.getElementById("wechat_acc").innerHTML = document.getElementById("wechat_input").value;
 					}
 					else {
 						alert("设置微信账号失败: " + data.error_msg);
@@ -51,11 +97,25 @@ if (!isset($_COOKIE['isLogin']) || !$_COOKIE['isLogin']) {
 			function setAlipay()
 			{
 				var account = document.getElementById("alipay_input").value;
+				account = $.trim(account);
+				if (account.length <= 0) {
+					alert("输入的账号无效，请重新输入！");
+					document.getElementById("alipay_input").focus();
+					return;
+				}
+				
+				if (!confirm("一旦设置，将不能手动修改，是否继续设置支付宝支付账号？")) {
+					return;
+				}
 				
 				$.post("../php/payaccount.php", {"func":"setalipay","acc":account}, function(data){
 					
 					if (data.error == "false") {
 						alert("设置成功！");	
+						document.getElementById("alipay_unset").style.display = "none";
+						document.getElementById("alipay_set").style.display = "inline";
+						
+						document.getElementById("alipay_acc").innerHTML = document.getElementById("alipay_input").value;
 					}
 					else {
 						alert("设置支付宝账号失败: " + data.error_msg);
@@ -72,10 +132,44 @@ if (!isset($_COOKIE['isLogin']) || !$_COOKIE['isLogin']) {
 				var bank = document.getElementById("bank_name").value;
 				var branch = document.getElementById("bank_branch").value;
 				
-				$.post("../php/payaccount.php", {"func":"setbank","pwd":pwd1}, function(data){
+				name = $.trim(name);
+				if (name.length <= 0) {
+					alert("账户人姓名不能为空，请重新输入！");
+					document.getElementById("bank_user").focus();
+					return;
+				}
+				account = $.trim(account);
+				if (account.length <= 0) {
+					alert("银行卡号不能为空，请重新输入！");
+					document.getElementById("bank_account").focus();
+					return;
+				}
+				bank = $.trim(bank);
+				if (bank.length <= 0) {
+					alert("银行名不能为空，请重新输入！");
+					document.getElementById("bank_name").focus();
+					return;
+				}
+				branch = $.trim(branch);
+				if (branch.length <= 0) {
+					alert("开户支行不能为空，请重新输入！");
+					document.getElementById("bank_branch").focus();
+					return;
+				}
+				
+				if (!confirm("一旦设置，将不能手动修改，请确认您填写的内容正确，是否继续设置银行账号？")) {
+					return;
+				}
+				
+				$.post("../php/payaccount.php", {"func":"setbank","user":name,"acc":account,"bank":bank,"bra":branch}, function(data){
 					
 					if (data.error == "false") {
 						alert("设置成功！");	
+						document.getElementById("bank_unset").style.display = "none";
+						document.getElementById("bank_set").style.display = "inline";
+						
+						document.getElementById("bank_acc").innerHTML = document.getElementById("bank_user").value + " " + document.getElementById("bank_account").value
+																			+ " " + document.getElementById("bank_name").value + " " + document.getElementById("bank_branch").value;
 					}
 					else {
 						alert("设置失败: " + data.error_msg);
@@ -99,31 +193,33 @@ if (!isset($_COOKIE['isLogin']) || !$_COOKIE['isLogin']) {
         <div name="wechar">
 	        <hr>
 	        <h4>微信</h4>
-	        <div id='wechat_unset'>
+	        <div id='wechat_unset' style="display: <?php if ($isWechatSet) echo "none"; else echo "inline"; ?>" >
 		        <p>请输入您的微信账号</p>
 		        <input id='wechat_input' type="text" placeholder="请输入您的微信账号" />
 		        <input type="button" value="确认" onclick="setWechat()" />
 	        </div>
-	        <div id='wechat_set'>
+	        <div id='wechat_set' style="display: <?php if (!$isWechatSet) echo "none"; else echo "inline"; ?>" >
+		        <p>您的微信账号： <b id='wechat_acc'><?php echo $weAcc; ?></b></p>
 	        </div>
         </div>
         
         <div name="alipay">
 	        <hr>
 	        <h4>支付宝</h4>
-	        <div id='alipay_unset'>
+	        <div id='alipay_unset' style="display: <?php if ($isAlipaySet) echo "none"; else echo "inline"; ?>" >
 		        <p>请输入您的支付宝账号</p>
 		        <input id='alipay_input' type="text" placeholder="请输入您的支付宝账号" />
 		        <input type="button" value="确认" onclick="setAlipay()" />
 	        </div>
-	        <div id='alipay_set'>
+	        <div id='alipay_set' style="display: <?php if (!$isAlipaySet) echo "none"; else echo "inline"; ?>" >
+		        <p>您的支付宝账号： <b id='alipay_acc'><?php echo $aliAcc; ?></b></p>
 	        </div>
 	    </div>
         
         <div name="bank">
 	        <hr>
 	        <h4>银行账户</h4>
-	        <div id='bank_unset'>
+	        <div id='bank_unset' style="display: <?php if ($isBankSet) echo "none"; else echo "inline"; ?>" >
 		        <p>请输入您的银行卡信息，推荐使用建设银行账户</p>
 		        <input id='bank_user' type="text" placeholder="请输入您的账户姓名" />
 		        <br>
@@ -135,7 +231,8 @@ if (!isset($_COOKIE['isLogin']) || !$_COOKIE['isLogin']) {
 		        <br>
 		        <input type="button" value="确认" onclick="setBankAccount()" />
 	        </div>
-	        <div id='bank_set'>
+	        <div id='bank_set' style="display: <?php if (!$isBankSet) echo "none"; else echo "inline"; ?>" >
+		        <p id='bank_acc'><?php echo $bankAcc; ?></p>
 	        </div>
         </div>
     </body>
--- php/payaccount.php
+++ php/payaccount.php
@@ -7,25 +7,154 @@ if (!isset($_POST['func'])) {
 }
 
 if ("setwechat" == $_POST['func']) {
+	setWechat();
 }
 else if ("setalipay" == $_POST['func']) {
+	setAlipay();
 }
 else if ("setbank" == $_POST['func']) {
+	setBank();
 }
 
 function setWechat()
-{
+{	
+	session_start();
+	if (!$_SESSION["isLogin"]) {
+		echo json_encode(array('error'=>'true','error_code'=>'20','error_msg'=>'请先登录！'));
+		return;
+	}
 	
+	$con = connectToDB();
+	if (!$con)
+	{
+		echo json_encode(array('error'=>'true','error_code'=>'30','error_msg'=>'设置失败，请稍后重试！'));
+		return;
+	}
+	
+	$weAcc = trim(htmlspecialchars($_POST["acc"]));
+	if (strlen($weAcc) <= 0) {
+		echo json_encode(array('error'=>'true','error_code'=>'1','error_msg'=>'无效的微信账号，请重新输入！'));
+		return;
+	}
+	
+	createWechatTable();
+	
+	$userid = $_SESSION["userId"];
+	
+	$result = mysql_query("select * from WechatAccount where UserId='$userid'");
+	if (!$result) {
+		echo json_encode(array('error'=>'true','error_code'=>'31','error_msg'=>'查询微信账户失败，请稍后重试'));
+		return;
+	}
+	
+	if (mysql_num_rows($result) > 0) {
+		echo json_encode(array('error'=>'true','error_code'=>'2','error_msg'=>'您的微信账户已设置，请不要重复设置！'));
+		return;
+	}
+	
+	$res = mysql_query("insert into WechatAccount (UserId, WechatAcc) 
+							values('$userid', '$weAcc')");
+	if (!$res) {
+		echo json_encode(array('error'=>'true','error_code'=>'3','error_msg'=>'添加微信账户失败，请稍后重试！'));
+		return;
+	}
+	
+	echo json_encode(array('error'=>'false'));
 }
 
 function setAlipay()
-{
+{	
+	session_start();
+	if (!$_SESSION["isLogin"]) {
+		echo json_encode(array('error'=>'true','error_code'=>'20','error_msg'=>'请先登录！'));
+		return;
+	}
 	
+	$con = connectToDB();
+	if (!$con)
+	{
+		echo json_encode(array('error'=>'true','error_code'=>'30','error_msg'=>'设置失败，请稍后重试！'));
+		return;
+	}
+	
+	$alipayAcc = trim(htmlspecialchars($_POST["acc"]));
+	if (strlen($alipayAcc) <= 0) {
+		echo json_encode(array('error'=>'true','error_code'=>'1','error_msg'=>'无效的支付宝账号，请重新输入！'));
+		return;
+	}
+	
+	createAlipayTable();
+	
+	$userid = $_SESSION["userId"];
+	
+	$result = mysql_query("select * from AlipayAccount where UserId='$userid'");
+	if (!$result) {
+		echo json_encode(array('error'=>'true','error_code'=>'31','error_msg'=>'查询支付宝账户失败，请稍后重试'));
+		return;
+	}
+	
+	if (mysql_num_rows($result) > 0) {
+		echo json_encode(array('error'=>'true','error_code'=>'2','error_msg'=>'您的支付宝账户已设置，请不要重复设置！'));
+		return;
+	}
+	
+	$res = mysql_query("insert into AlipayAccount (UserId, AlipayAcc) 
+							values('$userid', '$alipayAcc')");
+	if (!$res) {
+		echo json_encode(array('error'=>'true','error_code'=>'3','error_msg'=>'添加支付宝账户失败，请稍后重试！'));
+		return;
+	}
+	
+	echo json_encode(array('error'=>'false'));
 }
 
 function setBank()
 {
+	session_start();
+	if (!$_SESSION["isLogin"]) {
+		echo json_encode(array('error'=>'true','error_code'=>'20','error_msg'=>'请先登录！'));
+		return;
+	}
 	
+	$con = connectToDB();
+	if (!$con)
+	{
+		echo json_encode(array('error'=>'true','error_code'=>'30','error_msg'=>'设置失败，请稍后重试！'));
+		return;
+	}
+	
+	$name = trim(htmlspecialchars($_POST["user"]));
+	$acc = trim(htmlspecialchars($_POST["acc"]));
+	$bank = trim(htmlspecialchars($_POST["bank"]));
+	$branch = trim(htmlspecialchars($_POST["bra"]));
+	if (strlen($name) <= 0 || strlen($acc) <= 0 || strlen($bank) <= 0 || strlen($branch) <= 0) {
+		echo json_encode(array('error'=>'true','error_code'=>'1','error_msg'=>'您有未填写的项目，请重新输入！'));
+		return;
+	}
+
+	$userid = $_SESSION["userId"];
+	
+	createBankAccountTable();
+	
+	$result = mysql_query("select * from BankAccount where UserId='$userid'");
+	if (!$result) {
+		echo json_encode(array('error'=>'true','error_code'=>'31','error_msg'=>'查询银行账户失败，请稍后重试'));
+		return;
+	}
+	
+	if (mysql_num_rows($result) > 0) {
+		echo json_encode(array('error'=>'true','error_code'=>'2','error_msg'=>'您的银行账户已设置，请不要重复设置！'));
+		return;
+	}
+	
+	$res = mysql_query("insert into BankAccount (UserId, BankAcc, AccName, BankName, BankBranch)
+							values('$userid', '$acc', '$name', '$bank', '$branch')");
+	if (!$res) {
+		echo json_encode(array('error'=>'true','error_code'=>'3','error_msg'=>'添加银行账户失败，请稍后重试！'));
+		return;
+	}
+	
+	echo json_encode(array('error'=>'false'));
 }
 
 ?>
