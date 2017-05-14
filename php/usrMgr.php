84e49401c25c2d6ec3d679de2d5691042f0eda88
liyuebi <dragon8763@sina.com>
2017-05-14 08:18:57 +0800

    search user in admin

--- php/usrMgr.php
+++ php/usrMgr.php
@@ -4,6 +4,18 @@ include "database.php";
 include "regtest.php";
 include "constant.php";
 
+
+if (!isset($_POST['func'])) {
+	exit('非法访问！');
+}
+
+if ("addUser" == $_POST['func']) {
+	addUser();
+}
+else if ("queryUser" == $_POST['func']) {
+	queryUser();	
+}
+
 // admin login
 // 判断是否登录
 /*
@@ -14,102 +26,145 @@ if (!$_SESSION["isLogin"]) {
 }
 */
 
-$phonenum = trim(htmlspecialchars($_POST['phonenum']));
-$username = htmlspecialchars($_POST['name']);
-
-// 验证电话号码
-if (!isValidCellPhoneNum($phonenum)) {
-	echo json_encode(array('error'=>'true','error_code'=>'1','error_msg'=>'电话号码格式不对，请重新填写！'));
-	return;
-}
-
-$con = connectToDB();
-if (!$con)
+function addUser()
 {
-	echo json_encode(array('error'=>'true','error_code'=>'30','error_msg'=>'数据库连接失败，请稍后重试！','sql_error'=>mysql_error()));
-	return;
-}
-else 
-{		  
-	$result = createClientTable();
-	if (!$result) {
-		echo json_encode(array('error'=>'true','error_code'=>'31','error_msg'=>'用户表创建失败，请稍后重试！','sql_error'=>mysql_error())); 
-		return;
-	}
-	$result = createCreditTable();
-	if (!$result) {
-		echo json_encode(array('error'=>'true','error_code'=>'32','error_msg'=>'积分表创建失败，请稍后重试！','sql_error'=>mysql_error())); 
+	$phonenum = trim(htmlspecialchars($_POST['phonenum']));
+	$username = trim(htmlspecialchars($_POST['name']));
+	
+	// 验证电话号码
+	if (!isValidCellPhoneNum($phonenum)) {
+		echo json_encode(array('error'=>'true','error_code'=>'1','error_msg'=>'电话号码格式不对，请重新填写！'));
 		return;
 	}
 	
-	$userid = 0;
-	
-	$sql = "select * from ClientTable where PhoneNum='$phonenum'";
-	$result = mysql_query($sql, $con);
-	$newuserid = 0;
-	$now = time();
-	if (!$result) {
-		echo json_encode(array('error'=>'true','error_code'=>'1','error_msg'=>'账号查询出错，请稍后重试！','sql_error'=>mysql_error()));
+	$con = connectToDB();
+	if (!$con)
+	{
+		echo json_encode(array('error'=>'true','error_code'=>'30','error_msg'=>'数据库连接失败，请稍后重试！','sql_error'=>mysql_error()));
 		return;
 	}
-	else {
-		$num = mysql_num_rows($result);
-		if ($num == 0) {
-			$pwd = md5('000000');
-			$pwd = password_hash($pwd, PASSWORD_DEFAULT);
-			$result = mysql_query("insert into ClientTable (PhoneNum, Name, Password, RegisterTime, ReferreeId)
-				VALUES('$phonenum', '$username', '$pwd', '$now', '$userid')");
-			if (!$result) {
-				echo json_encode(array('error'=>'true','error_code'=>'2','error_msg'=>'创建账号失败，请稍后重试！','sql_error'=>mysql_error()));
-				return;
-			}
-			else {
-				$newuserid = mysql_insert_id();
-			}
-		}
-		else {
-			echo json_encode(array('error'=>'true','error_code'=>'3','error_msg'=>'该手机号已经注册过！','sql_error'=>mysql_error()));
-			return;			
-		}
-	}
-		
-	if (0 != $newuserid) {
-		$result = mysql_query("select * from Credit where UserId='$newuserid'");
+	else 
+	{		  
+		$result = createClientTable();
 		if (!$result) {
-			echo json_encode(array('error'=>'true','error_code'=>'4','error_msg'=>'积分查询出错，请稍后重试！','sql_error'=>mysql_error()));
+			echo json_encode(array('error'=>'true','error_code'=>'31','error_msg'=>'用户表创建失败，请稍后重试！','sql_error'=>mysql_error())); 
+			return;
+		}
+		$result = createCreditTable();
+		if (!$result) {
+			echo json_encode(array('error'=>'true','error_code'=>'32','error_msg'=>'积分表创建失败，请稍后重试！','sql_error'=>mysql_error())); 
+			return;
+		}
+		
+		$userid = 0;
+		
+		$sql = "select * from ClientTable where PhoneNum='$phonenum'";
+		$result = mysql_query($sql, $con);
+		$newuserid = 0;
+		$now = time();
+		if (!$result) {
+			echo json_encode(array('error'=>'true','error_code'=>'1','error_msg'=>'账号查询出错，请稍后重试！','sql_error'=>mysql_error()));
 			return;
 		}
 		else {
 			$num = mysql_num_rows($result);
 			if ($num == 0) {
-				$result = mysql_query("insert into Credit (UserId)
-					VALUES('$newuserid')");
+				$pwd = md5('000000');
+				$pwd = password_hash($pwd, PASSWORD_DEFAULT);
+				$result = mysql_query("insert into ClientTable (PhoneNum, Name, Password, RegisterTime, ReferreeId)
+					VALUES('$phonenum', '$username', '$pwd', '$now', '$userid')");
 				if (!$result) {
-// 					echo json_encode(array('error'=>'true','error_code'=>'5','error_msg'=>'创建积分失败，请稍后重试！','sql_error'=>mysql_error()));
-// 					return;
+					echo json_encode(array('error'=>'true','error_code'=>'2','error_msg'=>'创建账号失败，请稍后重试！','sql_error'=>mysql_error()));
+					return;
 				}
 				else {
-// 					echo "Register success<br />";
+					$newuserid = mysql_insert_id();
 				}
-			}					
+			}
 			else {
+				echo json_encode(array('error'=>'true','error_code'=>'3','error_msg'=>'该手机号已经注册过！','sql_error'=>mysql_error()));
+				return;			
 			}
 		}
+			
+		if (0 != $newuserid) {
+			$result = mysql_query("select * from Credit where UserId='$newuserid'");
+			if (!$result) {
+				echo json_encode(array('error'=>'true','error_code'=>'4','error_msg'=>'积分查询出错，请稍后重试！','sql_error'=>mysql_error()));
+				return;
+			}
+			else {
+				$num = mysql_num_rows($result);
+				if ($num == 0) {
+					$result = mysql_query("insert into Credit (UserId)
+						VALUES('$newuserid')");
+					if (!$result) {
+	// 					echo json_encode(array('error'=>'true','error_code'=>'5','error_msg'=>'创建积分失败，请稍后重试！','sql_error'=>mysql_error()));
+	// 					return;
+					}
+					else {
+	// 					echo "Register success<br />";
+					}
+				}					
+				else {
+				}
+			}
+		}
+		
+		echo json_encode(array('error'=>'false'));
+		
+		// 更新统计数据
+		include "../php/func.php";
+		insertRecommendStatistics(0);
+		
+		mysql_close($con);
+	/*
+		$home_url = '../html/home.php';
+		header('Location: ' . $home_url);
+	*/
+	
+		return;
+	}
+}
+
+function queryUser()
+{
+	$userid = trim(htmlspecialchars($_POST['uid']));	
+	$con = connectToDB();
+	if (!$con)
+	{
+		echo json_encode(array('error'=>'true','error_code'=>'30','error_msg'=>'数据库连接失败，请稍后重试！','sql_error'=>mysql_error()));
+		return;
+	}
+
+
+	$res = mysql_query("select * from ClientTable where UserId='$userid'");
+	if (!$res) {
+		echo json_encode(array('error'=>'true','error_code'=>'1','error_msg'=>'查找用户失败，请稍后重试！','sql_error'=>mysql_error()));
+		return;
 	}
 	
-	echo json_encode(array('error'=>'false'));
+	if (mysql_num_rows($res) <= 0) {
+		echo json_encode(array('error'=>'true','error_code'=>'2','error_msg'=>'查找不到您输入的用户，请重新输入！','sql_error'=>mysql_error()));
+		return;		
+	}
 	
-	// 更新统计数据
-	include "../php/func.php";
-	insertRecommendStatistics(0);
+	$row = mysql_fetch_assoc($res);
 	
-	mysql_close($con);
-/*
-	$home_url = '../html/home.php';
-	header('Location: ' . $home_url);
-*/
-
-	return;
+	$credit = 0;
+	$vault = 0;
+	$dvault = 0;
+	$res1 = mysql_query("select * from Credit where UserId='$userid'");
+	if ($res1 && mysql_num_rows($res1) > 0) {
+		$row1 = mysql_fetch_assoc($res1);
+		$credit = $row1["Credits"];
+		$vault = $row1["Vault"];
+		$dvault = $row1["DVault"];
+	}
+	
+	echo json_encode(array('error'=>'false','nickname'=>$row["NickName"], 		
+				'id'=>$row["UserId"],'phone'=>$row["PhoneNum"],'name'=>$row["Name"],
+				'IDNum'=>$row["IDNum"],'lvl'=>$row["Lvl"],'credit'=>$credit,'vault'=>$vault,'dvault'=>$dvault));
 }
 
 ?>
