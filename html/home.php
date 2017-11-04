afce5e25e5afb477e231c152546884e4f38e9c43
liyuebi <dragon8763@sina.com>
2017-11-04 19:23:40 +0800

    add carousel to home page

--- html/home.php
+++ html/home.php
@@ -86,9 +86,10 @@ if ($con) {
 		<link rel="stylesheet" type="text/css" href="../css/mystyle.css" />
 <!-- 		<link rel="stylesheet" type="text/css" href="../css/luara.left.css"/> -->
 		
-		<script src="../js/jquery-1.8.3.min.js"></script>
+		<script src="../js/jquery-3.2.1.min.js"></script>
 		<script src="../js/jquery.luara.0.0.1.min.js"></script>
 		<script src="../js/scripts.js"></script>
+		<script src="../js/bootstrap-3.3.7/bootstrap.min.js"></script>
 		<script type="text/javascript">
 			
 			$(document).ready(function(){
@@ -156,10 +157,45 @@ if ($con) {
  			<a class="banner_info_data" href='me.php'>我的资料</a></p>
 		</div>
 -->
+<!--
         <div width="100%">
             <img src="../img/lian-post.jpg" width="100%" />
         </div>
-	        
+-->
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+		    <ol class="carousel-indicators">
+		    	<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="5"></li>
+		    </ol>
+		
+		    <!-- Wrapper for slides -->
+		    <div class="carousel-inner" role="listbox">
+		    	<div class="item active">
+			        <img src="../img/intro1.jpg" alt="...">
+			    </div>
+			    <div class="item">
+			        <img src="../img/intro2.jpg" alt="...">
+			    </div>
+			    <div class="item">
+			        <img src="../img/intro3.jpg" alt="...">
+			    </div>
+			    <div class="item">
+			        <img src="../img/intro4.jpg" alt="...">
+			    </div>
+			    <div class="item">
+			        <img src="../img/intro5.jpg" alt="...">
+			    </div>
+			    <div class="item">
+			        <img src="../img/intro6.jpg" alt="...">
+			    </div>
+		    </div>
+	    </div>
+	     
 		<div>
 			<table class="t1" border="1" align="center" style="margin-top: 5px; border: 1px solid #e7e7e7;" rules="none">
 				<tr>
