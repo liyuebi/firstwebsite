fed22337dc067aa307813b4b554e655ca4c75ba1
liyuebi <dragon8763@sina.com>
2017-11-21 22:47:03 +0800

    modify admin home display

--- admin/adminhome.php
+++ admin/adminhome.php
@@ -13,18 +13,22 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		
 		<title>连物网管理系统</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="initial-scale=1.0">
 		<meta name="description" content="">
 		<meta name="author" content="">
 		
 		<link rel="stylesheet" type="text/css" href="../css/bootstrap-3.3.7/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="../css/adminlte/AdminLTE.min.css" />
 		<link rel="stylesheet" type="text/css" href="../css/adminlte/skins/_all-skins.min.css" />
+		<!-- Font Awesome -->
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
 		
 		<script src="../js/jquery-3.2.1.min.js" ></script>
 		<script src="../js/bootstrap-3.3.7/bootstrap.min.js"></script>
 <!-- 		<script src="../js/slimScroll/jquery.slimscroll.min.js"></script> -->
 <!-- 		<script src="../js/fastclick/fastclick.min.js"></script> -->
+		<!-- Bootstrap 3.3.7 -->
+		<!-- <script src="../js/bootstrap-3.3.7/bootstrap.min.js"></script> -->
 		<script src="../js/adminlte/adminlte.min.js"></script>
 		<script type="text/javascript">
 			
@@ -48,139 +52,138 @@
 			}
 		</script>
 	</head>
-	<body class="hold-transition skin-blue">
-		<div style="padding: 10px 10px 0 5px; height: 100%; float: left; border-right: 1px solid black; display: none;">
-			<ul style="list-style: none; padding: 0">
-<!-- 				<li><a href="companymgr.html">企业管理</a></li> -->
-<!-- 				<li><a href="productmgr.php">产品管理</a></li> -->
-				<li><a href="usermgr.php">用户管理</a></li>
-				<li><a href="ordermgr.php">订单管理</a></li>
-<!-- 				<li><a href="rechargemgr.php">充值管理</a></li> -->
-<!-- 				<li><a href="withdrawmgr.php">取现管理</a></li> -->
-				<li><a href="configmgr.php">配置管理</a></li>
-				<li><a href="statistics.php">统计数据</a></li>
-<!-- 				<li><a href="configRwdRate.php">配置动态拨比</a></li> -->
-				<li><a href="postmgr.php">公告管理</a></li>
-				<li><a href="adminmgr.php">管理员账号维护</a></li>
-			</ul>
-		</div>
-		<div style="display: inline; float: left; padding: 10px 0 0 10px;" >
-		</div>
-		
+	<body class="hold-transition skin-blue sidebar-mini">		
 		<div class="wrapper">
-		<aside class="main-sidebar" style="overflow-y:auto;">
-			<section class="sidebar">
-				<ul class="sidebar-menu" data-widget="tree">
-					<li class="treeview">
-		        	    <a href="javascript:void(0)">
-				            <i class="fa fa-home"></i><span>用户管理</span><i class="fa fa-angle-left pull-right"></i>
-			            </a>
-			            <ul class="treeview-menu">
-		            		<li onclick="makecss(this)" data-id="index_User">
-		            			<a href='usermgr.php' target='rightContent'><i class="fa fa-circle-o"></i>ID 查询</a>
-							</li>	            
-		            		<li onclick="makecss(this)" data-id="index_User1">
-		            			<a href='usermgr1.php' target='rightContent'><i class="fa fa-circle-o"></i>条件查询</a>
-							</li>	            
-		            	</ul>
-					</li>
-					<li class="treeview">
-		        	    <a href="javascript:void(0)">
-				            <i class="fa fa-home"></i><span>订单管理</span><i class="fa fa-angle-left pull-right"></i>
-			            </a>
-			            <ul class="treeview-menu">
-		            		<li onclick="makecss(this)" data-id="index_Order">
-		            			<a href='ordermgr.php' target='rightContent'><i class="fa fa-circle-o"></i>新用户订单</a>
-							</li>
-		            		<li onclick="makecss(this)" data-id="index_Order3">
-		            			<a href='ordermgr1.php' target='rightContent'><i class="fa fa-circle-o"></i>话费订单</a>
-							</li>
-		            		<li onclick="makecss(this)" data-id="index_Order1">
-		            			<a href='ordermgr2.php' target='rightContent'><i class="fa fa-circle-o"></i>油费订单</a>
-							</li>
-<!--
-		            		<li onclick="makecss(this)" data-id="index_Order2">
-		            			<a href='ordermgr2.php' target='rightContent'><i class="fa fa-circle-o"></i>订单查询</a>
-							</li>	            
--->
-		            	</ul>
-					</li>
-					<li class="treeview">
-		        	    <a href="javascript:void(0)">
-				            <i class="fa fa-home"></i><span>线下商家</span><i class="fa fa-angle-left pull-right"></i>
-			            </a>
-			            <ul class="treeview-menu">
-<!--
-		            		<li onclick="makecss(this)" data-id="index_Statis">
-		            			<a href='statistics.php' target='rightContent'><i class="fa fa-circle-o"></i>商家管理</a>
-							</li>	            
--->
-		            		<li onclick="makecss(this)" data-id="index_OlShopApply">
-		            			<a href='olsreviewmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>待审核商家</a>
-							</li>	            
-		            		<li onclick="makecss(this)" data-id="index_OlShopWithdraw">
-		            			<a href='withdrawmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>提现申请</a>
-							</li>	            
-		            	</ul>
-					</li>
-					<li class="treeview">
-		        	    <a href="javascript:void(0)">
-				            <i class="fa fa-home"></i><span>配置管理</span><i class="fa fa-angle-left pull-right"></i>
-			            </a>
-			            <ul class="treeview-menu">
-		            		<li onclick="makecss(this)" data-id="index_Config">
-		            			<a href='configmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>全局配置</a>
-							</li>	            
-<!--
-		            		<li onclick="makecss(this)" data-id="index_Config">
-		            			<a href='lvl_configmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>等级配置</a>
-							</li>	            
--->
-		            	</ul>
-					</li>
-					<li class="treeview">
-		        	    <a href="javascript:void(0)">
-				            <i class="fa fa-home"></i><span>统计数据</span><i class="fa fa-angle-left pull-right"></i>
-			            </a>
-			            <ul class="treeview-menu">
-		            		<li onclick="makecss(this)" data-id="index_Statis">
-		            			<a href='statistics.php' target='rightContent'><i class="fa fa-circle-o"></i>总统计</a>
-							</li>	            
-		            		<li onclick="makecss(this)" data-id="index_dayStatis">
-		            			<a href='statistics_day.php' target='rightContent'><i class="fa fa-circle-o"></i>按日统计</a>
-							</li>	            
-		            	</ul>
-					</li>
-					<li>
-		        	    <a href="postmgr.php" target='rightContent'>
-				            <i class="fa fa-home"></i><span>公告管理</span><i class="fa fa-angle-left pull-right"></i>
-			            </a>
-<!--
-			            <ul class="treeview-menu">
-		            		<li onclick="makecss(this)" data-id="index_Poster">
-		            			<a href='postmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>订单查询</a>
-							</li>	            
-		            	</ul>
--->
-					</li>
-					<li> <!-- class="treeview"> -->
-		        	    <a href="adminmgr.php" target="rightContent">
-				            <i class="fa fa-home"></i><span>管理员账号维护</span><i class="fa fa-angle-left pull-right"></i>
-			            </a>
-<!--
-			            <ul class="treeview-menu">
-		            		<li onclick="makecss(this)" data-id="index_Account">
-		            			<a href='adminmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>订单查询</a>
-							</li>	            
-		            	</ul>
--->
-					</li>
-				</ul>
+			<header class="main-header">
+			    <!-- Logo -->
+			    <a href="index2.html" class="logo">
+			    	<!-- mini logo for sidebar mini 50x50 pixels -->
+					<span class="logo-mini"><b>连</b></span>
+					<!-- logo for regular state and mobile devices -->
+					<span class="logo-lg"><b>连物网</b>后台</span>
+			    </a>
+				<!-- Header Navbar: style can be found in header.less -->
+		    	<nav class="navbar navbar-static-top">
+		        	<!-- Sidebar toggle button-->
+		        	<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+		        		<span class="sr-only">Toggle navigation</span>
+		        	</a>
+	    		</nav>
+			</header>			
+			
+			<aside class="main-sidebar">
+				<section class="sidebar">
+					<ul class="sidebar-menu" data-widget="tree">
+						<li class="treeview">
+			        	    <a href="javascript:void(0)">
+					            <i class="fa fa-user"></i><span>用户管理</span><i class="fa fa-angle-left pull-right"></i>
+				            </a>
+				            <ul class="treeview-menu">
+			            		<li onclick="makecss(this)" data-id="index_User">
+			            			<a href='usermgr.php' target='rightContent'><i class="fa fa-circle-o"></i>ID 查询</a>
+								</li>	            
+			            		<li onclick="makecss(this)" data-id="index_User1">
+			            			<a href='usermgr1.php' target='rightContent'><i class="fa fa-circle-o"></i>条件查询</a>
+								</li>	            
+			            	</ul>
+						</li>
+						<li class="treeview">
+			        	    <a href="javascript:void(0)">
+					            <i class="fa fa-money"></i><span>订单管理</span><i class="fa fa-angle-left pull-right"></i>
+				            </a>
+				            <ul class="treeview-menu">
+			            		<li onclick="makecss(this)" data-id="index_Order">
+			            			<a href='ordermgr.php' target='rightContent'><i class="fa fa-circle-o"></i>新用户订单</a>
+								</li>
+			            		<li onclick="makecss(this)" data-id="index_Order3">
+			            			<a href='ordermgr1.php' target='rightContent'><i class="fa fa-circle-o"></i>话费订单</a>
+								</li>
+			            		<li onclick="makecss(this)" data-id="index_Order1">
+			            			<a href='ordermgr2.php' target='rightContent'><i class="fa fa-circle-o"></i>油费订单</a>
+								</li>
+	<!--
+			            		<li onclick="makecss(this)" data-id="index_Order2">
+			            			<a href='ordermgr2.php' target='rightContent'><i class="fa fa-circle-o"></i>订单查询</a>
+								</li>	            
+	-->
+			            	</ul>
+						</li>
+						<li class="treeview">
+			        	    <a href="javascript:void(0)">
+					            <i class="fa fa-shopping-cart"></i><span>线下商家</span><i class="fa fa-angle-left pull-right"></i>
+				            </a>
+				            <ul class="treeview-menu">
+	<!--
+			            		<li onclick="makecss(this)" data-id="index_Statis">
+			            			<a href='statistics.php' target='rightContent'><i class="fa fa-circle-o"></i>商家管理</a>
+								</li>	            
+	-->
+			            		<li onclick="makecss(this)" data-id="index_OlShopApply">
+			            			<a href='olsreviewmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>待审核商家</a>
+								</li>	            
+			            		<li onclick="makecss(this)" data-id="index_OlShopWithdraw">
+			            			<a href='withdrawmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>提现申请</a>
+								</li>	           
+			            	</ul>
+						</li>
+						<li class="treeview">
+			        	    <a href="javascript:void(0)">
+					            <i class="fa fa-edit"></i><span>配置管理</span><i class="fa fa-angle-left pull-right"></i>
+				            </a>
+				            <ul class="treeview-menu">
+			            		<li onclick="makecss(this)" data-id="index_Config">
+			            			<a href='configmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>全局配置</a>
+								</li>	            
+	<!--
+			            		<li onclick="makecss(this)" data-id="index_Config">
+			            			<a href='lvl_configmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>等级配置</a>
+								</li>	            
+	-->
+			            	</ul>
+						</li>
+						<li class="treeview">
+			        	    <a href="javascript:void(0)">
+					            <i class="fa fa-bar-chart-o"></i><span>统计数据</span><i class="fa fa-angle-left pull-right"></i>
+				            </a>
+				            <ul class="treeview-menu">
+			            		<li onclick="makecss(this)" data-id="index_Statis">
+			            			<a href='statistics.php' target='rightContent'><i class="fa fa-circle-o"></i>总统计</a>
+								</li>	            
+			            		<li onclick="makecss(this)" data-id="index_dayStatis">
+			            			<a href='statistics_day.php' target='rightContent'><i class="fa fa-circle-o"></i>按日统计</a>
+								</li>	            
+			            	</ul>
+						</li>
+						<li>
+			        	    <a href="postmgr.php" target='rightContent'>
+					            <i class="fa fa-envelope-o"></i><span>公告管理</span><!-- <i class="fa fa-angle-left pull-right"></i> -->
+				            </a>
+	<!--
+				            <ul class="treeview-menu">
+			            		<li onclick="makecss(this)" data-id="index_Poster">
+			            			<a href='postmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>订单查询</a>
+								</li>	            
+			            	</ul>
+	-->
+						</li>
+						<li> <!-- class="treeview"> -->
+			        	    <a href="adminmgr.php" target="rightContent">
+					            <i class="fa fa-eye"></i><span>管理员账号维护</span><!-- <i class="fa fa-angle-left pull-right"></i> -->
+				            </a>
+	<!--
+				            <ul class="treeview-menu">
+			            		<li onclick="makecss(this)" data-id="index_Account">
+			            			<a href='adminmgr.php' target='rightContent'><i class="fa fa-circle-o"></i>订单查询</a>
+								</li>	            
+			            	</ul>
+	-->
+						</li>
+					</ul>
+				</section>
+			</aside>
+			<section class="content-wrapper right-side" id="riframe">
+				<iframe id='rightContent' name='rightContent' width="100%"></iframe>
 			</section>
-		</aside>
-		<section class="content-wrapper right-side" id="riframe" style="margin: 0px; margin-left: 250px;">
-			<iframe id='rightContent' name='rightContent' width="100%" ></iframe>
-		</section>
 		</div>
 		
     </body>
