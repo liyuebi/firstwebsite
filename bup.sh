257b945af38128219228a568ded496a2e6fda38a
liyuebi <dragon8763@sina.com>
2018-05-12 20:13:22 +0800

    add backup shell script for backup everyday

---
+++ bup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#backup
+
+timestamp="`date +%y%m%d%H%M`"
+path="/root/dump"
+
+if [ ! -d "$path" ] ;then
+	exit 1
+elif [ ! -x "$path" ] ;then
+	exit 2
+fi
+
+folderpath=${path}"/"${timestamp}
+mkdir $folderpath
+
+#dump database
+sqldumpfile=${path}"/"${timestamp}"/"${timestamp}".sql"
+mysqldump -u root -p"123456789" mifeng_db > $sqldumpfile
+
+#copy license pic
+cp -a ./olLicensePic $folderpath 
+
+#copy qrCode pic
+cp -a ./olqrc $folderpath
+
+#copy poster pic
+cp -a ./poster $folderpath
+
+#copy product package pic
+cp -a ./pPackPic $folderpath
+
+#compress folder
+cd $path
+tar czf ${timestamp}".tar.gz" $timestamp
